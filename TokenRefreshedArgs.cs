3baa6ce9d50d696cda87792e3f1fc3c380a5a1fa
Sawer <danilch4023@gmail.com>
2023-01-05 17:23:35 +0500

    dotnet7

--- TokenRefreshedArgs.cs
+++ TokenRefreshedArgs.cs
@@ -1,4 +1,25 @@
-﻿namespace Twitcher.API.Events;
+﻿namespace Twitcher.API;
+
+/// <summary>Contains new tokens</summary>
+public class TokenRefreshedArgs : EventArgs
+{
+    /// <summary>New access tokens</summary>
+    public string AccessToken { get; set; }
+    /// <summary>New refresh tokens</summary>
+    public string RefreshToken { get; set; }
+    /// <summary>Twitch id of the token owner</summary>
+    public string UserId { get; set; }
+
+    /// <summary>Access and refresh tokens in 'access:refresh' format</summary>
+    public string Tokens => AccessToken + ':' + RefreshToken;
+
+    internal TokenRefreshedArgs(string accessToken, string refreshToken, string userId)
+    {
+        AccessToken = accessToken;
+        RefreshToken = refreshToken;
+        UserId = userId;
+    }
+}
 
 /// <summary>Contains new tokens and owner</summary>
 public class APITokenRefreshedArgs : EventArgs
